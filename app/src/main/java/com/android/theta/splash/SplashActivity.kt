2457936dc09121b418b3cff702cbfeee57f0671a
Sougandh Mp <sougandhmp@gmail.com>
2021-06-06 14:48:18 +0200

    Initial commit

---
+++ app/src/main/java/com/android/theta/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.android.theta.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.theta.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
