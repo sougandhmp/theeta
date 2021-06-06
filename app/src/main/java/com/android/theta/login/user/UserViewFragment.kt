fa7b3550541d3061e4d534a6081dbb176ffb56ef
akhil babu <akhilbabu92@gmail.com>
2021-06-06 22:39:31 +0530

    Added user view + nav

---
+++ app/src/main/java/com/android/theta/login/user/UserViewFragment.kt
@@ -0,0 +1,34 @@
+package com.android.theta.login.user
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.android.theta.R
+
+class UserViewFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = UserViewFragment()
+    }
+
+    private lateinit var viewModel: UserViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.user_view_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+
+}
