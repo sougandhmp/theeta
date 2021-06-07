7da5d55637622bafe250469033e82c9b69c728d5
akhil babu <akhilbabu92@gmail.com>
2021-06-07 23:05:42 +0530

    added user view to item list and example

---
+++ app/src/main/java/com/android/theta/user/ItemListFragment.kt
@@ -0,0 +1,40 @@
+package com.android.theta.user
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.android.theta.databinding.ItemListFragmentBinding
+
+
+class ItemListFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ItemListFragment()
+    }
+
+    private lateinit var viewModel: IteamListModel
+    private lateinit var binding: ItemListFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = ItemListFragmentBinding.inflate(layoutInflater, container, false)
+        viewModel = ViewModelProvider(this).get(IteamListModel::class.java)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(IteamListModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+
+
+
+}
--- app/src/main/java/com/android/theta/user/UserViewFragment.kt
+++ app/src/main/java/com/android/theta/user/UserViewFragment.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.android.theta.R
+import com.android.theta.databinding.UserViewFragmentBinding
+
 
 class UserViewFragment : Fragment() {
 
@@ -15,19 +18,35 @@ class UserViewFragment : Fragment() {
     }
 
     private lateinit var viewModel: UserViewModel
+    private lateinit var binding: UserViewFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.user_view_fragment, container, false)
+        binding = UserViewFragmentBinding.inflate(layoutInflater, container, false)
+        viewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(UserViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+    override fun onStart() {
+        super.onStart()
+        navigateToVendor()
+    }
+
+    private fun navigateToVendor() {
+
+        binding.action1.setOnClickListener {
+
+            findNavController().navigate(R.id.action_userView_to_itemList)
+        }
+
     }
 
 
