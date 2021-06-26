c859602836ebd09fc051f8e5095d219fe15552a6
akhil babu <akhilbabu92@gmail.com>
2021-06-27 00:39:06 +0530

    Added dialog

--- app/src/main/java/com/android/theta/vendor/main/VendorViewFragment.kt
+++ app/src/main/java/com/android/theta/vendor/main/VendorViewFragment.kt
@@ -1,15 +1,18 @@
 package com.android.theta.vendor.main
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.android.theta.R
 import com.android.theta.commons.observe
 import com.android.theta.databinding.VendorViewFragmentBinding
 import com.android.theta.vendor.model.VendorItem
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -37,14 +40,10 @@ class VendorViewFragment : Fragment() {
             }
             lifecycleOwner = this@VendorViewFragment
         }
+        var thiscontext = context
+        context?.let { onlineSwitch(it) }
         return binding.root
     }
-
-    override fun onStart() {
-        super.onStart()
-        onlineSwitch()
-    }
-
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -59,16 +58,53 @@ class VendorViewFragment : Fragment() {
         list ?: return
         vendorViewAdapter.submitList(list)
     }
-    private fun onlineSwitch() {
+
+
+    private fun onlineSwitch(context: Context) {
         binding.switchButton.setOnCheckedChangeListener { buttonView, isChecked ->
             run {
                 if (isChecked)
-                    buttonView.text = getString(R.string.store_online)
+                    dialog(
+                        context,
+                        buttonView,
+                        getString(R.string.warning),
+                        "You really want  to go online?",
+                        getString(R.string.store_online)
+                    )
+
                  else
-                    buttonView.text = getString(R.string.store_offline)
+                    dialog(
+                        context,
+                        buttonView,
+                        getString(R.string.warning),
+                        "You really want  to go offline ?",
+                        getString(R.string.store_offline)
+                    )
 
             }
         }
+    }
+
+    private fun dialog(
+        context: Context,
+        buttonView: CompoundButton,
+        title: String,
+        message: String,
+        text: String
+    ) {
+        MaterialAlertDialogBuilder(context)
+            .setTitle(title)
+            .setMessage(message)
+            .setNeutralButton("cancel") { dialog, which ->
+                // Respond to neutral button press
+            }
+            .setNegativeButton("decline") { dialog, which ->
+                // Respond to negative button press
+            }
+            .setPositiveButton("accept") { dialog, which ->
+                buttonView.text = text
+            }
+            .show()
     }
 
 }
